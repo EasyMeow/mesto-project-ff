bdd5863c6c22c76d3ddfe4082014e8ff4ff6779b
Mikhail Trofimov <easymeowus@gmail.com>
2024-06-26 09:46:25 +0400

    Implemented cards loading and self removing

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardInfo, remove) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = cardInfo.link;
+    cardElement.querySelector('.card__image').alt = `Фотография: ${cardInfo.name}`;
+    cardElement.querySelector('.card__title').textContent = cardInfo.name;
 
-// @todo: Функция создания карточки
+    const removeButton = cardElement.querySelector('.card__delete-button');
+    removeButton.addEventListener('click', () => remove(cardElement));
 
-// @todo: Функция удаления карточки
+    return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+function removeCard(card) {
+    card.remove();
+}
+
+initialCards.forEach(function (item) {
+    placesList.append(createCard(item, removeCard));
+});
