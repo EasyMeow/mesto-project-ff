6b976d58f2f50f378d1c6bb39809c644d6d1af8d
Mikhail Trofimov <easymeowus@gmail.com>
2024-06-27 18:32:39 +0400

    Configure html-webpack-plugin and clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: {main: './src/index.js'},
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
@@ -15,12 +17,18 @@ module.exports = {
         open: true
     },
     module: {
-    rules: [
-        {
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: '/node_modules/'
-        }
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html' // путь к файлу index.html
+        }),
+        new CleanWebpackPlugin(),
     ]
-}
 }
