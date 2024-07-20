1330bccb984f1099d9dbdddb7637499fc5da279c
Mikhail Trofimov <easymeowus@gmail.com>
2024-07-20 11:46:47 +0300

    Added submit event for editing profile

--- src/index.js
+++ src/index.js
@@ -9,17 +9,34 @@ const editProfilePopup = document.querySelector('.popup_type_edit');
 const newCardButton = document.querySelector('.profile__add-button');
 const newCardPopup = document.querySelector('.popup_type_new-card');
 
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+const formElement = editProfilePopup.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput =document.querySelector('.popup__input_type_description');
 
 editProfilePopup.addEventListener('click', evt =>  closeByCrossOrOverlay(evt, editProfilePopup));
 newCardPopup.addEventListener('click', evt =>  closeByCrossOrOverlay(evt, newCardPopup));
 
 editButton.addEventListener('click', evt => {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileDescription.textContent;
     openPopup(editProfilePopup);
 });
 
 newCardButton.addEventListener('click', evt => {
     openPopup(newCardPopup);
 });
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    closePopup(editProfilePopup);
+}
 
 const openCardPopup = (card) => {
     const popup = document.querySelector('.popup_type_image');
